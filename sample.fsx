d856c7b7af902e2153b3f9ecbf88f7dac3ec8302
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2020-03-11 09:03:53 +0100

    Cleanup

--- sample.fsx
+++ sample.fsx
@@ -26,20 +26,27 @@ let allData =
     |> Seq.map Daily.Load
     |> Seq.collect(fun data -> data.Rows)
 
-let byCountry =
+let cleanedData =
     allData
-    |> Seq.filter(fun row -> row.``Country/Region`` <> "Others")
-    |> Seq.groupBy(fun a -> a.``Country/Region``, a.``Last Update``.Date)
-    |> Seq.map(fun (key, rows) ->
-        key, rows |> Seq.sumBy(fun row -> row.Confirmed.GetValueOrDefault 0))
-    |> Seq.groupBy(fun (key, _) -> fst key)
-    |> Seq.map(fun (country, rows) ->
-        country.Trim(), rows |> Seq.map(fun ((_, date), confirmed) -> date, confirmed))
+    |> Seq.map (fun row ->
+        row.``Country/Region``.Trim(),
+        row.``Last Update``.Date,
+        row.Confirmed.GetValueOrDefault 0)
+    |> Seq.filter (fun (country,_,_) -> country <> "Others")
+
+let byCountry =
+    cleanedData
+    |> Seq.groupBy (fun (country,date,_) -> country, date)
+    |> Seq.map (fun (group, rows) ->
+        group, rows |> Seq.sumBy (fun (_,_,confirmed) -> confirmed))
+    |> Seq.groupBy (fun ((country,_), _) -> country)
+    |> Seq.map (fun (country, rows) ->
+         country, rows |> Seq.map (fun ((_, date), confirmed) -> date, confirmed))
 
 let topTen =
     byCountry
     |> Seq.sortByDescending(fun (_, rows) ->
-        let _, confirmed = rows |> Seq.last
+        let (_, confirmed) = rows |> Seq.last
         confirmed)
     |> Seq.skip 1
     |> Seq.take 10
