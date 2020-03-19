b202e02543670fbd120a5c8ac47b62a501c664ac
Harvtronix <harvtronix@geodude.pkmn>
2020-03-19 14:34:38 -0400

    create-react-app with some customizations

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
