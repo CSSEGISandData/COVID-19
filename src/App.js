be8d3fcbee9b62291317d022f84f3941bc14c135
Harvtronix <harvtronix@geodude.pkmn>
2020-03-19 14:42:56 -0400

    Deploy attempt

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <p>
+        Hello, world!
+      </p>
     </div>
   );
 }
