c10c8a157aa13418ce0df9436e63c8168074dc0b
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-03-19 16:29:50 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="COVID-19",
+    version="nan",
+    description="COVID-19 data analysis",
+    author="Cigan Oliviu-David",
+    author_email="ciganoliviudavid@gmail.com",
+    license="MIT",
+    packages=["covid_19"],
+)
