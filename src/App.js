5553268d0bd6a31543bf78d5231acc329701c068
Eric O <eric@ejdigitalmedia.com>
2020-03-19 22:12:31 -0400

    Now parsing csv data into 2D array

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import csvParse from 'csv-parse';
 
 function App() {
-
-  const [csvData, setCsvData] = useState(null);
+  const [dataset, setDataset] = useState(null);
 
   useEffect(() => {
+    // Set up CSV parser
+    const parseCsvData = (data) => {
+      const csvParser = csvParse();
+      const csvArr = [];
+
+      // Use the readable stream api
+      csvParser.on('readable', function(){
+        let record;
+        while ((record = csvParser.read())) {
+          csvArr.push(record)
+        }
+      });
+      // Catch any error
+      csvParser.on('error', function(err){
+        console.error(err.message)
+      });
+      // Send data to read to the stream
+      csvParser.write(data);
+
+      // Return the CSV as a 2D array
+      return csvArr;
+    };
+
     axios.get('/data/time_series_19-covid-Confirmed.csv')
       .then(function (response) {
-        setCsvData(response.data);
+        setDataset(parseCsvData(response.data));
       })
       .catch(function (error) {
         console.log(error);
@@ -22,7 +45,7 @@ function App() {
         I heard you like data!
       </p>
       <p>
-        { csvData ? csvData : 'Loading...' }
+        { dataset ? JSON.stringify(dataset) : 'Loading...' }
       </p>
     </div>
   );
