69a6fa33fd49f7ba5086fd07dd8d0d74ed2c1445
Len Greski <len@greskilabs.com>
2020-04-17 13:21:29 -0400

    first commit to submit to main repo, R subdirectory and code to analyze and standardize column names across daily reports files.

---
+++ R/graphCovid19-cobbAndFulton.R
@@ -0,0 +1,19 @@
+#
+# graph covid-19 confirmed cases
+# 
+
+library(lubridate)
+georgia <- data[data$Province_State == "Georgia",]
+cobb <- georgia[georgia$Admin2 %in% c("Cobb"),]
+cobb$date <- mdy(cobb$date)
+
+plot(cobb$date,cobb$Confirmed,type = "h",cex = .5,ylim=c(0,2000),
+     main="Cobb County GA - COVID-19 Confirmed Cases",
+     xlab = "Date",ylab = "Confirmed Cases")
+
+fulton <- georgia[georgia$Admin2 %in% c("Fulton"),]
+fulton$date <- mdy(fulton$date)
+
+plot(fulton$date,fulton$Confirmed,type = "h",cex = .5,ylim=c(0,2000),
+     main="Fulton County GA - COVID-19 Confirmed Cases",
+     xlab = "Date",ylab = "Confirmed Cases")
---
+++ R/readDailyReportsData.R
@@ -0,0 +1,62 @@
+#
+# read JHU covid-19 data
+#
+# 2020-04-17 lgreski 
+
+# edit this line to reflect where data is stored on local machine 
+directory <- "./csse_covid_19_data/csse_covid_19_daily_reports/"
+
+# read all files in directory
+theFiles <- list.files(path=directory,pattern="*.csv",full.names = TRUE)
+filenames <- list.files(path=directory,pattern="*.csv")
+
+# read and clean column information, add missing columns for earliest data
+# for details, see ./R/analyzeColumnNamesByDay.R in github repository 
+library(lubridate)
+
+dataList <- lapply(1:length(theFiles),function(x){
+  y <-read.csv(theFiles[x],stringsAsFactors=FALSE)
+  # clean column names and add missing columns 
+  fileDate <- mdy(substr(filenames[x],1,10))
+
+    # common processing across all files
+  colnames(y) <- gsub("\\.","_",colnames(y))
+  colnames(y) <- sub("ï__","",colnames(y)) 
+
+  # processing specific to first batch of files
+  if(fileDate < mdy("02/01/2020")) {
+    y$Province_State <- NA
+    y$Lat <- NA
+    y$Long_ <- NA
+    y$Active <- NA
+    y$Admin2 <- NA
+    y$FIPS <- NA 
+    y$Combined_Key <- NA 
+  } else if (fileDate < mdy("03/01/2020")){
+    # cleaning specific to first wave of files
+    y$Lat <- NA
+    y$Long_ <- NA
+    y$Active <- NA
+    y$Admin2 <- NA
+    y$FIPS <- NA 
+    y$Combined_Key <- NA 
+  } else if(fileDate < mdy("03/22/2020")) {
+       colnames(y) <- sub("Latitude","Lat",colnames(y))
+       colnames(y) <- sub("Longitude","Long_",colnames(y))
+       y$Active <- NA
+       y$Admin2 <- NA 
+       y$FIPS <- NA
+       y$Combined_Key <- NA 
+  }
+  # extract date from file name, assign to date column because
+  # Last_Update field format varies day by day and a good programmer is 
+  # a lazy programmer
+  y$date <- substr(filenames[x],1,10)
+  y
+  })
+
+# check number of columns
+columnCount <- unlist(lapply(1:length(theFiles),function(x) length(names(dataList[[x]]))))
+
+data <- do.call(rbind,dataList)
+
