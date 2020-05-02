d796081a920bf4cddef4c777c441679ed8cbc0aa
Len Greski <len@greskilabs.com>
2020-05-02 08:00:45 -0400

    increase ylimit argument

--- R/plotMultipleStatesCovidCases.R
+++ R/plotMultipleStatesCovidCases.R
@@ -9,15 +9,15 @@ source("./R/plotCovidUSCounties.R")
 source("./R/multiplot.R")
 # GA
 gaCounties <- c("Cobb","Fulton","Cherokee","DeKalb","Gwinnett")
-p1 <- plotCovidUSCounties(data,"Georgia",gaCounties)
+p1 <- plotCovidUSCounties(data,"Georgia",gaCounties,yLimit=c(0,3500))
 
 # IL
 ilCounties<- c("Cook","Lake","Will","McHenry","DuPage","Kane")
-p3 <- plotCovidUSCounties(data,"Illinois",ilCounties)
+p3 <- plotCovidUSCounties(data,"Illinois",ilCounties,yLimit=c(0,3500))
 
 # CO
 coCounties <- c("Denver","Arapahoe","Jefferson","Douglas","Adams")
-p2 <- plotCovidUSCounties(data,"Colorado",coCounties,yLimit=c(0,2000))
+p2 <- plotCovidUSCounties(data,"Colorado",coCounties,yLimit=c(0,3500))
 
 # print combined plot
 multiplot(p1,p2,cols = 1)
