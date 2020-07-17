ad549727ece0fa698f57247ce24e980fcd815916
Len Greski <len@greskilabs.com>
2020-07-17 09:23:01 -0400

    adjust group label in plot

--- R/plotFatalityRateUSStates.R
+++ R/plotFatalityRateUSStates.R
@@ -10,12 +10,13 @@ plotCovidFatalityRateUSStates <- function(data,states,yLimits=NULL){
   aSubset %>% group_by(.,Province_State,date) %>%
     summarise(., Confirmed = sum(Confirmed),
               Deaths = sum(Deaths),
-              Case_Fatality_Ratio = Deaths * 100000 / (Confirmed) ) -> df
+              Case_Fatality_Ratio = Deaths * 100000 / (Confirmed) ) %>%
+    rename(.,State = Province_State) -> df
   df$date <- mdy(df$date)
   asOfDate <- max(df$date)
-  ggplot(df, aes(date,Case_Fatality_Ratio, group = Province_State)) + 
-    geom_line(aes(group = Province_State), color = "grey80") +
-    geom_point(aes(color = Province_State)) + scale_x_date(date_breaks = "2 days") +
+  ggplot(df, aes(date,Case_Fatality_Ratio, group = State)) + 
+    geom_line(aes(group = State), color = "grey80") +
+    geom_point(aes(color = State)) + scale_x_date(date_breaks = "2 days") +
     easy_rotate_x_labels(angle = 45, side = "right")  +
     scale_y_continuous(limits = yLimits) + 
     labs(x = "Date",
