c9831d6063bdb0a598b56bd8f294df8d37e7aac1
Len Greski <len@greskilabs.com>
2020-07-18 17:18:30 -0400

    add code to read all state files and combine

--- R/readUSDailyReports.R
+++ R/readUSDailyReports.R
@@ -3,8 +3,24 @@
 #
 
 directory <- "./csse_covid_19_data/csse_covid_19_daily_reports_us/"
+theFiles <- list.files(path=directory,pattern="*.csv",full.names = TRUE)
+filenames <- list.files(path=directory,pattern="*.csv")
 
-data <- read.csv(paste0(directory,"05-15-2020.csv"),header = TRUE)
+data <- read.csv(paste0(directory,"07-15-2020.csv"),header = TRUE)
 
-summary(data$Recovered)
 
+dataList <- lapply(1:length(theFiles),function(x){
+  y <-read.csv(theFiles[x],stringsAsFactors=FALSE)
+  # clean column names and add missing columns 
+  fileDate <- mdy(substr(filenames[x],1,10))
+  
+  # common processing across all files
+  colnames(y) <- gsub("\\.","_",colnames(y))
+  colnames(y) <- sub("ï__","",colnames(y)) 
+  y
+}) 
+
+# check number of columns, should be 18 for all files 
+table(unlist(lapply(1:length(theFiles),function(x) length(names(dataList[[x]])))))
+
+data <- do.call(rbind,dataList)
